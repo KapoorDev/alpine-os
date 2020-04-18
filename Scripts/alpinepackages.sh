17b86bd3c48add00b4b3f504d90984967baa7cfe
KapoorDev <59065079+KapoorDev@users.noreply.github.com>
2020-04-18 20:10:19 +0530

    Add files via upload

---
+++ Scripts/alpinepackages.sh
@@ -0,0 +1,6 @@
+#by hachnhaul
+#this script will install all packages in alpine
+clear
+echo -e "\e[34m[*]\t\e[32mInstalling all the Necessary packages.\e[0m";
+echo -e "\e[34m[*]\t\e[32mIt can take upto 15-20 minutes.\e[0m";
+(apk add bash-completion bash-doc;apk --update add openjdk7-jre php7 php7-fpm php7-opcache php7-gd php7-mysqli php7-zlib php7-curl bash git curl wget ruby ruby-rdoc python python2 python3 py-pip;gem install lolcat;apk add terminator;gem install lolcat) > /dev/null
